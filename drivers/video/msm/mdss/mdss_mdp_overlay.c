a3d0303a714fc6ad38307b5a3dab6802d153c7cf
DerRomtester <dieb.stefan.96@gmail.com>
2015-10-17 13:58:23 +0200

    Merge branch 'LA.BF.1.1.3_rb1.2' into upstream-LA.BF.1.1.3_rb1.2

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1194,6 +1194,7 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd,
 		mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_BEGIN);
 		mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_READY);
 		mutex_lock(ctl->shared_lock);
+		mutex_lock(ctl->wb_lock);
 	}
 
 	mutex_lock(&mdp5_data->ov_lock);
@@ -1316,9 +1317,11 @@ commit_fail:
 		wake_up_all(&mfd->kickoff_wait_q);
 	}
 	mutex_unlock(&mdp5_data->ov_lock);
-	if (ctl->shared_lock)
+	if (ctl->shared_lock) {
+		mutex_unlock(ctl->wb_lock);
 		mutex_unlock(ctl->shared_lock);
-	mdss_iommu_ctrl(0);
+	}
+
 	ATRACE_END(__func__);
 
 	return ret;
