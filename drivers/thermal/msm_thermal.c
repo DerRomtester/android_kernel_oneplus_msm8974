3fabed9f51f9f695d664aff7e454d415faaa3fad
DerRomtester <dieb.stefan.96@gmail.com>
2017-02-08 22:21:31 +0100

    Revert "msm_thermal: cpufreq.h is not used in here so we dont need it also increase temp threshold to 42"
    This reverts commit 971b67bb17f713b244879cd7c30f78104eac3280.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -22,8 +22,9 @@
 #include <linux/msm_thermal.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
+#include <mach/cpufreq.h>
 
-unsigned int temp_threshold = 42;
+unsigned int temp_threshold = 40;
 module_param(temp_threshold, int, 0644);
 
 static struct thermal_info {
