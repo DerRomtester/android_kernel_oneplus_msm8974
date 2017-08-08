1f6ad7119e965befc1b2a03189506af6c905d248
DerRomtester <dieb.stefan.96@gmail.com>
2017-08-08 19:30:39 +0200

    Revert "cpuquiet: introduce"
    This reverts commit 62d357eb12504e99566320d00e8ce4f0cdc9c635.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2640,7 +2640,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!se) {
 		update_rq_runnable_avg(rq, rq->nr_running);
-		__inc_nr_running(rq);
+		inc_nr_running(rq);
 	}
 	hrtick_update(rq);
 }
